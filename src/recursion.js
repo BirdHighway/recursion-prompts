f9507771851a793d38881660bb08302e447cc071
Stephen Hyde <stephen@birdhighway.com>
2020-12-28 13:18:46 -0500

    Implement exponent function

--- src/recursion.js
+++ src/recursion.js
@@ -74,7 +74,19 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp < 0) {
+    return 1 / exponent(base, -1 * exp);
+  }
+  if (exp > 0 && exp % 2 === 0) {
+    var root = exponent(base, exp / 2);
+    return root * root;
+  }
+  return base * exponent( base, exp - 1);
 };
+
 
 // 8. Determine if a number is a power of two.
 // powerOfTwo(1); // true
